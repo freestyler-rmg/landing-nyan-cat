926fa1351fd6d2df9f19c2acd10b821f2b68127c
Roy Margasa <roy.margasa@gmail.com>
2025-04-16 18:01:54 +0700

    Add github pages pipe config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://freestyler-rmg.github.io',
+  base: 'landing-nyan-cat',
   vite: {
     plugins: [tailwindcss()]
   }
